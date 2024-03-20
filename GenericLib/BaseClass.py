7cd64129e5a9e8241273cfc22dd95edc7503069b
anand <anand.n@instavc.com>
2024-03-20 10:57:55 +0000

    Edited BaseClass.py

--- GenericLib/BaseClass.py
+++ GenericLib/BaseClass.py
@@ -10,7 +10,7 @@ class BaseClass(unittest.TestCase):
         self.logger = LogGen.loggen()
         # self.driver = webdriver.Chrome()
         # Choose the browser you want (Chrome, Firefox, Edge, etc.)
-        browser_name = "Chrome"
+        browser_name = "Edge"
 
         if browser_name == "Chrome":
             self.driver = webdriver.Chrome()
